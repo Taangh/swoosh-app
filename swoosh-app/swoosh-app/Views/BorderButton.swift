6fe00e550eab065c291b9b3ae39629d67a41b1a1
Damian <damian@Damians-MacBook-Pro.local>
2017-08-12 22:42:37 +0200

    First commit

---
+++ swoosh-app/swoosh-app/Views/BorderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BorderButton.swift
+//  swoosh-app
+//
+//  Created by Damian on 12.08.2017.
+//  Copyright © 2017 Damian. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+
+}
